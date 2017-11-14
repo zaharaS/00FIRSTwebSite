b2b51e77cce57110b25c0d222338b3a9b9fc4122
zaharaS <sashaxx@mail.ru>
2017-11-14 18:24:55 +0300

    JSc
    JSc

---
+++ 7 урок/javascript.js
@@ -0,0 +1,14 @@
+alert ("Привет я окно!");
+
+
+function fun1() {
+    var checkbox;
+    checkbox=document.getElementById("one");
+    if (checkbox.checked) {
+        alert("Выбран")
+    }
+    else {
+        alert("Не выбран");
+    }
+
+}
